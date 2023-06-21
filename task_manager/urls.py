58203156d6d6120d38e84ddab99ec82a9b8b8aa5
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-21 13:07:38 +0300

    Created static and css directories and updated static settings

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("manager.urls", namespace="tasks"))
+    path("", include("manager.urls", namespace="tasks")),
 ]
