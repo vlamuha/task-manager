a9dbbd691f7ada338a467d0e1b13c7d6b8e3aea6
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-21 12:51:30 +0300

    Added registration on admin site and modifed admin site

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from manager.models import Worker, Position, Task, TaskType
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_filter = ("task_type",)
+    search_fields = ("name",)
+
+
+admin.site.register(TaskType)
+
+admin.site.register(Position)
