3447b8db4ee3d78c9bc1fd7264469e22c8b42d62
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-21 12:30:06 +0300

    Added TaskType model and fixed models.Model in Position model

--- manager/models.py
+++ manager/models.py
@@ -2,9 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Position:
+class Position(models.Model):
     name = models.CharField(max_length=255)
 
 
 class Worker(AbstractUser):
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
