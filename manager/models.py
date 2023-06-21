2be28366d3d08a24fa21791044d0ebfd16afb95c
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-21 12:26:46 +0300

    Added Worker model and AUTH_USER_MODEL to settings

--- manager/models.py
+++ manager/models.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class Position:
     name = models.CharField(max_length=255)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
