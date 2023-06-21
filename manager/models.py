bedd951f5ee81233f8975b106bc869f2d2c5d143
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-21 12:35:38 +0300

    Added Task model

--- manager/models.py
+++ manager/models.py
@@ -12,3 +12,19 @@ class Worker(AbstractUser):
 
 class TaskType(models.Model):
     name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    deadline = models.DateTimeField(editable=True)
+    is_completed = models.BooleanField()
+    PRIORITY_CHOICES = [
+        ("low", "Low Priority"),
+        ("medium", "Medium Priority"),
+        ("high", "High Priority"),
+        ("urgent", "Urgent Priority")
+    ]
+    priority = models.CharField(choices=PRIORITY_CHOICES, max_length=10)
+    task_type = models.ForeignKey(TaskType, on_delete=CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
