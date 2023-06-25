ec4872d5651febfae7d33d33dfeba13ff162af3f
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-25 12:11:18 +0300

    Black to tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
