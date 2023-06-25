9150e20a66941b79abaa03dce2607cc3d76d5064
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-25 12:16:34 +0300

    Update workers/create to worker/create in urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -39,7 +39,7 @@ urlpatterns = [
     ),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
-    path("workers/create/", WorkerCreateView.as_view(), name="worker-create"),
+    path("worker/create/", WorkerCreateView.as_view(), name="worker-create"),
     path("workers/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
     path("workers/<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete"),
     path("positions/", PositionListView.as_view(), name="positions-list"),
